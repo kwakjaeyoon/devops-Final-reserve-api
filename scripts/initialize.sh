f9e6bf74727d2785774c6de53a58e28afa7dac33
Jaeminst <woals1245@gmail.com>
2022-06-01 14:14:32 +0900

    Fix: authbind is delete, pm2 to npm start

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 cd /home/ubuntu/super-g/server
-sudo apt update
-sudo apt install -y npm
+apt-get update
+apt-get upgrade -y
+apt-get dist-upgrade -y
+apt-get autoremove -y
+apt-get install nodejs npm git -y
+ln -s /usr/bin/nodejs /usr/bin/node
 npm install
-npm install pm2@latest -g
-sudo apt-get update
-sudo apt-get install authbind
-sudo touch /etc/authbind/byport/3000
-sudo chown ubuntu /etc/authbind/byport/3000
-sudo chmod 755 /etc/authbind/byport/3000
